fe7c00abce31a20bc66caa81f657087c566ebe90
Gabriel <gabrielvie25@outlook.com>
2022-06-05 19:06:20 -0300

    feat: Implementado função que conta quantidade de palavras camel case em uma determinada string

---
+++ desafio8.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'camelcase' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def camelcase(s):
+    pattern = re.compile(r'(^[a-z]|[A-Z])?[a-z]+')
+    results = pattern.findall(s)
+    return len(results)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
